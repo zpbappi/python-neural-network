cf16211c66bf449b9ea31f0cadf10e322817ecbf
zpbappi <zpbappi@gmail.com>
2016-04-11 10:14:00 +1000

    added binary and multiclass classifiers

--- model.py
+++ model.py
@@ -1,3 +1,6 @@
+import numpy as np
+from math_helper import MathHelper
+
 class Model:
     def __init__(self, thetas):
         def has_valid_dimensions(matrics):
@@ -15,3 +18,41 @@ class Model:
 
         if not has_valid_dimensions(thetas):
             raise(ValueError("Thetas should have dimensions like: (a,b), (b+1,c), (c+1,d)..."))
+
+        self._thetas = thetas
+        self._math_helper = MathHelper()
+
+    def evaluate(self, X_in):
+        X = np.asmatrix(X_in)
+        m, n = X.shape
+        theta_0_cols = self._thetas[0].shape[1]
+        if n+1 != theta_0_cols:
+            raise(ValueError("Input has {0} features and expected theta_0 to have {1} columns, but found {2} columns in theta_0.".format(n, n+1, theta_0_cols)))
+
+        activation = np.ones((m, n+1))
+        activation[:,1:] = X[:,:]
+        for theta in self._thetas:
+            ZT = np.dot(activation, np.transpose(theta))
+            Z_shape = ZT.shape
+            AT = np.ones((Z_shape[0], Z_shape[1]+1))
+            AT[:,1:] = self._math_helper.sigmoid(ZT)
+            activation = AT
+
+        return activation[:,1:]
+
+    def predict_binary_classification(self, X_in, positive_threshold_inclusive = 0.5):
+        hypothesis = self.evaluate(X_in)
+        if hypothesis.shape[1] > 1:
+            raise(ValueError("Output layer must have only one feature for binary classification, but found {0} features.".format(hypothesis.shape[1])))
+
+        return np.asmatrix((hypothesis >= positive_threshold_inclusive).astype(int))
+
+
+    def predict_multiclass_classification(self, X_in):
+        hypothesis = self.evaluate(X_in)
+        if hypothesis.shape[1] == 1:
+            raise(ValueError("Found only 1 feature in output layer. Please use binary classifier `model.predict_binary_classification(...)`."))
+        elif hypothesis.shape[1] == 2:
+            raise(ValueError("Found 2 features in output layer. Please consider using binary classifier with just one output feature."))
+
+        return np.asmatrix(hypothesis).argmax(1)
--- tests/model_tests.py
+++ tests/model_tests.py
@@ -20,3 +20,58 @@ class ModelTests(unittest.TestCase):
         with self.assertRaises(ValueError):
             Model([np.random.rand(5,4), np.random.rand(2, 5)])
         successful_model = Model([np.random.rand(10, 5), np.random.rand(10, 11), np.random.rand(5, 11), np.random.rand(3, 6)])
+
+    def test_model_should_take_a_single_input_row(self):
+        model = Model([np.random.rand(10, 5), np.random.rand(10, 11), np.random.rand(5, 11), np.random.rand(3, 6)])
+        model.evaluate([x for x in range(4)])
+
+    def test_model_should_also_take_multiple_rows(self):
+        model = Model([np.random.rand(10, 5), np.random.rand(10, 11), np.random.rand(5, 11), np.random.rand(3, 6)])
+        model.evaluate([[x for x in range(4)], [x**2 for x in range(4)]])
+
+    def test_evaluation_should_throw_for_invalid_input_dimensions(self):
+        model = Model([np.random.rand(10, 5), np.random.rand(10, 11), np.random.rand(5, 11), np.random.rand(3, 6)])
+        with self.assertRaises(ValueError):
+            model.evaluate([x for x in range(2)])
+
+    def test_evaluate_returns_correct_output_shape(self):
+        model = Model([np.random.rand(10, 5), np.random.rand(10, 11), np.random.rand(5, 11), np.random.rand(3, 6)])
+        result = model.evaluate([[r*5+c for c in range(4)] for r in range(100)])
+        actual = result.shape
+        expected = (100, 3)
+        self.assertEqual(actual, expected)
+
+    def test_evaulate_returns_correct_shapes_for_trivial_cases(self):
+        model = Model([np.random.rand(1,2), np.random.rand(1,2)])
+        result = model.evaluate(np.random.rand(1,1))
+        self.assertEqual(result.shape, (1,1))
+
+    def test_binary_classification_throws_on_more_than_one_output_feature(self):
+        model = Model([np.random.rand(4, 6), np.random.rand(3, 5)])
+        with self.assertRaises(ValueError):
+            model.predict_binary_classification(np.random.rand(10, 5))
+
+    def test_binary_classsification_should_out_row_vector_of_0_1_only(self):
+        model = Model([np.random.rand(4, 6), np.random.rand(1, 5)])
+        prediction = model.predict_binary_classification(np.random.rand(10, 5))
+        self.assertEqual(prediction.shape, (10, 1))
+        zeros = len(np.argwhere(prediction == 0))
+        ones = len(np.argwhere(prediction == 1))
+        self.assertEqual(zeros + ones, 10)
+
+    def test_multiclass_classification_throws_on_one_output_feature(self):
+        model = Model([np.random.rand(4, 6), np.random.rand(1, 5)])
+        with self.assertRaises(ValueError):
+            model.predict_multiclass_classification(np.random.rand(10, 5))
+
+    def test_multiclass_classification_throws_on_two_output_feature(self):
+        model = Model([np.random.rand(4, 6), np.random.rand(2, 5)])
+        with self.assertRaises(ValueError):
+            model.predict_multiclass_classification(np.random.rand(10, 5))
+
+    def test_multiclass_classification_returns_proper_output_shape_and_values(self):
+        model = Model([np.random.rand(4, 6), np.random.rand(3, 5)])
+        prediction = model.predict_multiclass_classification(np.random.rand(10, 5))
+        self.assertEqual(prediction.shape, (10, 1))
+        self.assertTrue(all(prediction >= 0))
+        self.assertTrue(all(prediction <= 2))
