f8ce7ca9d82aaa2255674ddf97a7494c97393249
zpbappi <zpbappi@gmail.com>
2016-04-12 15:06:37 +1000

    added learning curve generator

--- learningcurve.py
+++ learningcurve.py
@@ -1,21 +1,63 @@
 import numpy as np
 from neuralnetwork import NeuralNetwork
+from math_helper import MathHelper
 
 class LearningCurve:
-    def __init__(self, X_train, Y_train, X_cv, Y_cv):
+    def __init__(self, lambda_val, hidden_layer_sizes, X_train, Y_train, X_cv, Y_cv):
+        if lambda_val is None:
+            raise(ValueError("Lambda value is mandatory."))
+        if hidden_layer_sizes is None or len(hidden_layer_sizes) == 0:
+            raise(ValueError("Must provide a valid hidden layer size."))
         if X_train is None or Y_train is None or X_cv is None or Y_cv is None:
             raise(ValueError("Both training and cross validation data are mandatory for learning curve."))
 
-        X_train = np.asmatrix(X_train)
-        Y_train = np.asmatrix(Y_train)
-        X_cv = np.asmatrix(X_cv)
-        Y_cv = np.asmatrix(Y_cv)
+        self.X_train = np.asmatrix(X_train)
+        self.Y_train = np.asmatrix(Y_train)
+        self.X_cv = np.asmatrix(X_cv)
+        self.Y_cv = np.asmatrix(Y_cv)
+        self._lambda = lambda_val
+        self._hidden_layer_sizes = hidden_layer_sizes
+        self._helper = MathHelper()
 
-        if X_train.shape[0] != Y_train.shape[0]:
+
+        if self.X_train.shape[0] != self.Y_train.shape[0]:
             raise(ValueError("Training input and output data set should have same number of rows."))
-        if X_cv.shape[0] != Y_cv.shape[0]:
+        if self.X_cv.shape[0] != self.Y_cv.shape[0]:
             raise(ValueError("Cross validation input and output data set should have same number of rows."))
-        if X_train.shape[1] != X_cv.shape[1]:
+        if self.X_train.shape[1] != self.X_cv.shape[1]:
             raise(ValueError("Both training and cross validation input data set should have same number of features."))
-        if Y_train.shape[1] != Y_cv.shape[1]:
+        if self.Y_train.shape[1] != self.Y_cv.shape[1]:
             raise(ValueError("Both training and cross validation output data set should have same number of features."))
+
+    def _cost(self, Y, hypothesis, train_data_size, cost_regularization):
+        eps = np.finfo(float).eps
+        J = sum(-np.log(hypothesis[Y == 1] + eps))
+        J += sum(-np.log((1 + eps) - hypothesis[Y == 0]))
+        J /= train_data_size
+        J += cost_regularization
+        return J
+
+
+    def generate(self, indices = None):
+        m, n = self.X_train.shape
+        k = self.Y_train.shape[1]
+        m_cv = self.X_cv.shape[0]
+
+        nn = NeuralNetwork.init(self._lambda, n, k, self._hidden_layer_sizes)
+
+        indices = range(1, m+1) if indices is None else indices
+
+        for i in indices:
+            x_sub = self.X_train[:i,:]
+            y_sub = self.Y_train[:i,:]
+            model = nn.train(x_sub, y_sub)
+
+            cost_reg_train = nn.cost_regularization(model.thetas, i)
+            h_train = model.evaluate(x_sub)
+            error_train = self._cost(y_sub, h_train, i, cost_reg_train)
+
+            cost_reg_cv = nn.cost_regularization(model.thetas, m_cv)
+            h_cv = model.evaluate(self.X_cv)
+            error_cv = self._cost(self.Y_cv, h_cv, m_cv, cost_reg_cv)
+
+            yield (i, error_train, error_cv)
--- tests/learning_curve_test.py
+++ tests/learning_curve_test.py
@@ -6,20 +6,51 @@ from learningcurve import LearningCurve
 class LearningCurveTest(unittest.TestCase):
     def test_all_x_and_y_parameters_are_mandatory(self):
         with self.assertRaises(ValueError):
-            LearningCurve(None, None, None, None)
+            LearningCurve(0, [2], None, None, None, None)
         with self.assertRaises(ValueError):
-            LearningCurve(np.random.rand(10, 5), np.random.rand(10, 1), None, None)
+            LearningCurve(0, [10], np.random.rand(10, 5), np.random.rand(10, 1), None, None)
 
-        successful_lc = LearningCurve(np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+        successful_lc = LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+
+    def test_lambda_and_hidden_layer_sizes_are_mandatory(self):
+        with self.assertRaises(ValueError):
+            LearningCurve(None, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+        with self.assertRaises(ValueError):
+            LearningCurve(0, [], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+        with self.assertRaises(ValueError):
+            LearningCurve(0, None, np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+
+        successful_lc = LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
 
     def test_train_and_cv_data_should_be_in_proper_shape(self):
         with self.assertRaises(ValueError):
-            LearningCurve(np.random.rand(10, 5), np.random.rand(9, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+            LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(9, 1), np.random.rand(2, 5), np.random.rand(2, 1))
         with self.assertRaises(ValueError):
-            LearningCurve(np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 6), np.random.rand(2, 1))
+            LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 6), np.random.rand(2, 1))
         with self.assertRaises(ValueError):
-            LearningCurve(np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(3, 1))
+            LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(3, 1))
         with self.assertRaises(ValueError):
-            LearningCurve(np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 2))
+            LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 2))
 
-        successful_lc = LearningCurve(np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+        successful_lc = LearningCurve(0, [5], np.random.rand(10, 5), np.random.rand(10, 1), np.random.rand(2, 5), np.random.rand(2, 1))
+
+    def test_learning_curve_generate_should_return_generator(self):
+        import types
+        lc = LearningCurve(0, [3], np.random.rand(10, 5), (np.random.rand(10, 3) > 0.5).astype(int), np.random.rand(2, 5), (np.random.rand(2, 3) > 0.5).astype(int))
+        data_points = lc.generate()
+        self.assertIsInstance(data_points, types.GeneratorType)
+
+    def test_learning_curve_generates_proper_touples(self):
+        lc = LearningCurve(0, [3], np.random.rand(10, 5), (np.random.rand(10, 3) > 0.5).astype(int), np.random.rand(2, 5), (np.random.rand(2, 3) > 0.5).astype(int))
+        data_points = lc.generate()
+        first_point = data_points.__next__()
+        self.assertEqual(len(first_point), 3)
+        self.assertIsInstance(first_point[0], int)
+        self.assertEqual(first_point[0], 1)
+
+    def test_learning_curve_generator_should_work_with_custom_indices(self):
+        lc = LearningCurve(0, [3], np.random.rand(10, 5), (np.random.rand(10, 3) > 0.5).astype(int), np.random.rand(2, 5), (np.random.rand(2, 3) > 0.5).astype(int))
+        custom_indices = [1, 3, 4];
+        data_points = lc.generate(custom_indices)
+        data_sizes = [x for (x, error_train, error_cv) in data_points]
+        self.assertEqual(custom_indices, data_sizes)
